43c4d8d9af9f45677fbfb095a44d487f2c812402
Kathy Lenth <klenth@westminstercollege.edu>
2021-07-15 14:15:30 -0600

    dbmanager: reintegrating old styling and redesigning for consistency with lab app

--- dbmanager/templatetags/functional.py
+++ dbmanager/templatetags/functional.py
@@ -4,19 +4,26 @@ register = template.Library()
 
 
 @register.filter(name='map')
-def map(value, f):
+def map_elements(value, f):
     f = str(f)
 
     def map_element(x):
         if hasattr(x, f):
-            return getattr(x, f)()
+            attr = getattr(x, f)
+            return attr() if callable(attr) else attr
         elif f in globals():
             return globals()[f](x)
         else:
             raise ValueError('No function {} found for value of type {}'.format(f, type(x)))
 
-    for x in value:
-        yield map_element(x)
+    #for x in value:
+    #    yield map_element(x)
+    return map(map_element, value)
+
+
+@register.filter(name='tolist')
+def tolist(values):
+    return list(values)
 
 
 @register.filter(name='except')
